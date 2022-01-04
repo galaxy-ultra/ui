56dd932134d22a4061628c4386b58e73d512c961
Nguyễn Đức <autoclickvn@gmail.com>
2022-01-04 09:09:45 +0700

    Update export

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,13 @@
-export { Alert } from '@galaxy-ultra/alert';
-export { Breadcrumb } from '@galaxy-ultra/breadcrumb';
-export { Button } from '@galaxy-ultra/button';
-export { Card } from '@galaxy-ultra/card';
-export { Modal } from '@galaxy-ultra/modal';
-export { Switch } from '@galaxy-ultra/switch';
+import AlertUI from '@galaxy-ultra/alert';
+import BreadcrumbUI from '@galaxy-ultra/breadcrumb';
+import ButtonUI from '@galaxy-ultra/button';
+import CardUI from '@galaxy-ultra/card';
+import ModalUI from '@galaxy-ultra/modal';
+import SwitchUI from '@galaxy-ultra/switch';
+
+export const Alert = AlertUI;
+export const Breadcrumb = BreadcrumbUI;
+export const Button = ButtonUI;
+export const Card = CardUI;
+export const Modal = ModalUI;
+export const Switch = SwitchUI;
